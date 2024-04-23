26a91a65070f15eb20a0fa20af313d743991ab31
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-04-23 20:38:00 +0200

    příloha Topping naprogramována ve vlastní třídě

---
+++ pizzaApp/object/Topping.py
@@ -0,0 +1,16 @@
+# příloha na pizzu
+
+class Topping:
+    def __init__(self, name, price):
+        self.__name = name   #privátní jméno
+        self.__price = price
+
+    def get_name(self):
+        return self.__name
+    
+    def get_price(self):
+        return self.__price
+
+
+    #TODO Přidat možnost zdražení
+
