0d3707768ab567c35f7028356d3f5bdfcfbedcd3
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-04-23 21:00:44 +0200

    Class Pizza create pizza

---
+++ pizzaApp/object/Pizza.py
@@ -0,0 +1,32 @@
+from Price import Price
+class Pizza:
+    def __init__(self, name, toppings):
+        #todo přidat druh těsta (tenky, tlusty), přidat druh základu (rajče, smetana)
+        # Možnost přidat extra Topping
+        self.name = name
+        self.__toppings = toppings
+        self.__DOUGHT_PRICE = Price(5, 90)  # cena těsta jako konstanta
+
+    def get_name(self):
+        return self.name
+
+    def get_price(self):
+        result = self.__DOUGHT_PRICE.get_price()
+        for i in self.__toppings:
+            result += t.get_price().get_price()  # první get price cena a pak to druhé vrací tu samotnou hodnotu
+
+        return result
+
+    def __str__(self):
+        topps = ""
+        for t in self.__toppings:
+            topps += t.get_name() + ", "
+        #odstraneni posledni čarky
+
+        return f"""
+{self.name}                  {self.get_price()} $
+-----------------------------------------------
+{self.__toppings}
+"""
+
+
--- pizzaApp/object/Price.py
+++ pizzaApp/object/Price.py
@@ -1,5 +1,6 @@
 
 class Price:
+    # nadefinovat magic metod pro matematické operace. sečíst 2 hodnoty s celými a desetinými čísly todo
     def __init__(self, integer, fraction):
         self.__integer = integer
         self.__fraction = fraction
@@ -10,4 +11,3 @@ class Price:
     def __str__(self):
         return f"{self.__integer},{self.__fraction}"
 
-    
