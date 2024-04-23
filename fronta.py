376353b13777899aca553212a888b18d6071598e
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-04-23 19:06:14 +0200

    struktura fronty, nutno doplnit od učitele jeho první comit

---
+++ fronta.py
@@ -0,0 +1,31 @@
+# třída spojení datových struktur jako fronta
+
+class CharQueue:
+    def __init__(self, size):
+        self.size = size
+        self.queue = []
+    def is_empty(self):
+        return len(self.queue) == 0
+
+    def is_full(self):
+        return len(self.queue) == self.size
+
+    def enqueue(self, item):
+        if not self.is_full():
+            self.queue.append(item):
+            return True
+        return False
+
+    def dequeue(self): #vrátí první prvek
+        if not self.is_empty():
+            item = self.queue.pop(0) # pop s indegem vrátí první prvek 0 pokud nedám 0 vrátí poslední
+            return item
+        return None
+
+    def show(self):
+        if not self.is_empty():
+            pass
+
+
+
+
---
+++ main.py
@@ -0,0 +1,13 @@
+
+# první úkol datové struktury a popis fronta
+
+from fronta import fronta
+
+a = [1,2]
+b = fronta(a)
+print(b)
+
+#todo doplnit logiku od učitele
+
+
+
